22b897e0fc3888413b9d4346c7a12ad4e6ef6c41
jeremingo <58335558+jeremingo@users.noreply.github.com>
2023-12-23 18:47:04 +0200

    Set leading delimiter to 1

--- config.h
+++ config.h
@@ -11,7 +11,7 @@
 #define CLICKABLE_BLOCKS 1
 
 // Control whether a leading delimiter should be prepended to the status.
-#define LEADING_DELIMITER 0
+#define LEADING_DELIMITER 1
 
 // Control whether a trailing delimiter should be appended to the status.
 #define TRAILING_DELIMITER 0
